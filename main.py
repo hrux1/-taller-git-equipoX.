e3adbedaa36fa03fc306f673b32a16e048c83ce2
Stiven Royet <royetsteven@gmail.com>
2025-09-26 21:37:39 -0500

    Agregada función potencia

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 import math
 
-def raiz (c):
-    return math.sqrt(c)
+def raiz(c):
+    resultado = math.sqrt(c)
+    if resultado.is_integer():
+        return int(resultado)
+    return resultado
 
 def suma(a, b):
     return a + b
@@ -12,16 +15,15 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
-def multiplicación(a, b):
+def multiplicacion(a, b):
     return a * b
+
+def potencia(a, b):
+    return a ** b
 
 if __name__ == "__main__":
     print("Suma 5 + 3:", suma(5, 3))
     print("Resta 10 - 4:", resta(10, 4))
-
-    print("Multiplicación 2 x 6:", resta(2, 6))
-
-
-c = 9
-print(raiz(c))
-
+    print("Multiplicación 2 x 6:", multiplicacion(2, 6))
+    print("Raíz cuadrada de 9:", raiz(9))
+    print("Potencia 2 ^ 3:", potencia(2, 3))
